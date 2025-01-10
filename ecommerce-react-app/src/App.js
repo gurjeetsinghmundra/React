be4a99c4707468990fb137f3035d309895c74704
gurjeetsinghmundra <singhgurjeet101003@gmail.com>
2025-01-10 09:40:49 +0530

    Ecommerce

---
+++ ecommerce-react-app/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import { createBrowserRouter ,RouterProvider } from 'react-router-dom'
+import Product from './components/products/Product';
+import Navbar from './components/Navbar';
+
+// Creating routes
+
+//route pe product component mount ho raha hai
+const routes = createBrowserRouter([
+  { 
+    //array ke andhar object
+    path:"/products",
+    element:<><Navbar/><Product/></>
+  },
+  {
+    path:"/",
+    element:<Navbar/>
+  }
+])
+
+function App() {
+  return (
+    <div className="">
+
+    <RouterProvider router={routes} />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ ecommerce-react-app/src/components/products/Product.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Product() {
+  return (
+    <div>Product
+        <h1>Hii</h1>
+    </div>
+  )
+}
+
+export default Product
