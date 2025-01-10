984a5fd1560540ec95f84bfd8db199b206c65a6c
gurjeetsinghmundra <singhgurjeet101003@gmail.com>
2025-01-10 08:45:14 +0530

    Github Card

--- second_app/src/components/GithubCard.jsx
+++ second_app/src/components/GithubCard.jsx
@@ -3,7 +3,8 @@ import React, { useEffect, useState } from 'react'
 function GithubCard() {
 
 
-    let [githubData, setGithubData] = useState({});
+    let [githubData, setGithubData] = useState({}); //useState me object pass kiya hai
+    let [repositories, setRepositories] = useState([]); //useState me array pass kiya hai
 
     //  useEffect ko ek function aur ek array chahiye raheta hai
     useEffect(() => {
@@ -18,19 +19,63 @@ function GithubCard() {
                 console.log(githubData);
 
                 setGithubData(data);
+
+                //Repositories
+
+                fetch(data.repos_url)
+                    .then(repos => repos.json())
+
+                    //will get all the repo here
+                    .then(repos => {
+
+                        // console.log("Repos"+repos);
+                        setRepositories(repos);
+                    })
             })
     }, [])
 
     return (
         //React ko inline css nhi chahiye
         <div>
+            {/* Profile Start */}
             <h1>Github Profile</h1>
             <div className="card github-card">
-                <img src="..." className="card-img-top" alt="..." />
+                <img src={githubData.avatar_url} className="card-img-top" alt="..." />
                 <div className="card-body">
-                    <p className="card-text"></p>
+                    <p className="card-text">Name: {githubData.name}</p>
+                    <p className="card-text">Followers: {githubData.followers}</p>
+                    <p className="card-text">Following: {githubData.following}</p>
+                    <a href={githubData.html_url}>Click Here to Visit Github Profile</a>
                 </div>
             </div>
+
+            {/* Repositories Start */}
+
+           
+            <table className="table table-success table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Repository Name</th>
+                        <th scope="col">Repository Link</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        repositories.map((repoObj)=>{
+                            return( <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                            </tr>)
+                        })
+                    }
+                   
+                </tbody>
+            </table>
+
+            {/* Repositories End */}
+
         </div>
     )
 }
